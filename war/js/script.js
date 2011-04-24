3b0dc5c60aebed1f1552aa2273fda7524b644555
toshipon <toshipon123@gmail.com>
2011-04-24 20:38:38 +0900

    レイアウトざっくり

--- war/js/script.js
+++ war/js/script.js
@@ -1,3 +1,13 @@
 $(function(){
-	$("#jquery-test").html("jQuery is loaded");
+	$(document).ready(function (){
+		// contentsサイズ初期化
+		var body = $("body");
+		var side = $("div#sidemenu");
+		$("div#contents").width(body.width() - side.outerWidth(true));
+		
+		 if (GBrowserIsCompatible()) {
+			 var map = new GMap2(document.getElementById("map_canvas"));
+		     map.setCenter(new GLatLng(35.363663,136.459915), 13);
+		 }
+	});
 });
